27f9463c6399a014b2f1c46a934bebd9cc1fd060
agusTrih <ant3dong@gmail.com>
2020-07-03 18:36:36 +0700

    add header and textTransform

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./App.css";
 import CatsAge from "./pages/CatsAge/CatsAge";
 import TextTransform from "./pages/TextTransform/TextTransform";
 import BmiCalculator from "./pages/BmiCalculator/BmiCalculator";
+import Header from "./components/Header/Header";
 
 function App() {
     return (
         <div className="App">
+            <Header />
             <CatsAge />
             <TextTransform />
             <BmiCalculator />
